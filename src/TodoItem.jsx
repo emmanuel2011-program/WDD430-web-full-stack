fa36d4be0246d14e11edc659ec0da8c59f128531
Emmanuel <okoyee61@gmail.com>
2025-09-16 05:45:47 +0100

    added item

---
+++ src/TodoItem.jsx
@@ -0,0 +1,19 @@
+export function TodoItem({id, completed, title, toggleTodo, deleteTodo}){
+   return <li>
+            <label>
+              <input 
+              type="checkbox" 
+              checked={completed}
+              readOnly
+              onChange={e => toggleTodo(id, e.target.checked)}
+              />
+              {title}
+            </label>
+            <button
+              className="btn btn-danger"
+              onClick={() => deleteTodo(id)}
+            >
+              Delete
+            </button>
+          </li> 
+}
